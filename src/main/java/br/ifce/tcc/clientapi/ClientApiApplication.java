bc0dea3fb52dd81687a8ca68711fe980465f6992
Matheus Holanda Matos <matheus.holandam@gmail.com>
2022-06-09 22:15:40 -0300

    Setting up project to be a eureka service

--- src/main/java/br/ifce/tcc/clientapi/ClientApiApplication.java
+++ src/main/java/br/ifce/tcc/clientapi/ClientApiApplication.java
@@ -2,8 +2,10 @@ package br.ifce.tcc.clientapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ClientApiApplication {
 
 	public static void main(String[] args) {
