ad31b21912988e3cd6c1cecd515246117e0b86bd
Matheus Holanda Matos <matheus.holandam@gmail.com>
2022-06-12 21:47:43 -0300

    Setting up client-api to work with netflix zuul

--- src/main/java/br/ifce/tcc/clientapi/controller/ClientController.java
+++ src/main/java/br/ifce/tcc/clientapi/controller/ClientController.java
@@ -9,14 +9,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.ifce.tcc.clientapi.model.Client;
 import br.ifce.tcc.clientapi.repository.ClientRepository;
 
 @RestController
-@RequestMapping("/v1/clients")
 public class ClientController extends AbstractController {
 
 	@Autowired ClientRepository clientRepository;
